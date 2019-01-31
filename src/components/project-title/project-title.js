96cb227f7f9cc7af6b5af049fd5ab4bf10afa515
Igor <sobol.dominus@gmail.com>
2019-01-31 16:03:30 +0200

    Start work at the chenge title todos function.

--- src/components/project-title/project-title.js
+++ src/components/project-title/project-title.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './project-title.css';
 
-const ProjectHeader = () => {
+export default class ProjectHeader extends Component {
+ 
+  state = { titile: 'For Home!' };
+
+  chengetitle = (text) => {
+    this.setState((state) => {
+      return {
+        title: state.title
+      };
+    });
+  };
+
+  render() {
     return (
       <div className="project-title">
           <span><i className="icon-list"></i></span> 
-          <span>For Home</span> 
           <span>
-            <button className="btn">
+            { this.state.titile }
+          </span> 
+          <span>
+
+            <button className="btn"
+                    onClick={ this.chengetitle('New title!') }>
               <i className="icon-pencil"></i>
             </button>
+
             <button className="btn">
               <i className="icon-bin"></i>
             </button>
+
           </span>
       </div>
     );
+  };
+
 };
-export default ProjectHeader;
