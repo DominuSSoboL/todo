00eb6ed4830df7fba84a03747e9b4a8205b9cbe6
IgorSobol <sobol.dominus@gmail.com>
2019-11-27 22:17:10 +0200

    Added supported scss sintaxis and replace stylesheets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import './index.css';
+//import App from './components/app';
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import './stylesheets/application.scss';
+
+import TodoListPage from '../src/pages/todo_list_page';
+ReactDOM.render(<TodoListPage />, document.getElementById('root'));
+  
